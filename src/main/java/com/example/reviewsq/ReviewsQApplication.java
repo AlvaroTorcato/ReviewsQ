79154009ed94b176816eea1cb7f9f3556f66d21b
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-11-09 09:15:01 +0000

    Initial commit

---
+++ src/main/java/com/example/reviewsq/ReviewsQApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewsQApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reviewsq/ReviewsQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewsq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsQApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
